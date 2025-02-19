1f7919a404a4d03314831df7af66a71d73ab633a
Ilya <bkmy43@googlemail.com>
2025-02-19 16:59:55 +0100

    changes to support config in UI

--- etl/etl_load_stats_1d.py
+++ etl/etl_load_stats_1d.py
@@ -3,36 +3,49 @@ from etl.main import get_stats_for_country
 from etl.db_resource import db_connection
 from sqlalchemy import text
 
-class Stats1dConfig(Config):
-    iso2: str = None
-    date_from: str = None
-    date_to: str = None
 
-@asset
-def etl_load_stats_1d(context, config: Stats1dConfig):    
+class Stats1dConfig(Config):
+    iso2: str
+    date_from: str
+    date_to: str
+
+
+@asset(
+    config_schema={
+        "iso2": str,
+        "date_from": str,
+        "date_to": str
+    },
+    required_resource_keys={"db"}  # Ensuring the database resource is available
+)
+def etl_load_stats_1d(context):
     """
     Extracts daily statistics for a given country and date range,
     and loads them into the database.
     """
-    if None in (config.iso2, config.date_from, config.date_to):
+    config = context.op_config  # Fetch configuration from UI
+
+    if None in (config["iso2"], config["date_from"], config["date_to"]):
         raise ValueError("All ETL Job parameters must be set")
 
-    context.log.info(f"Running ETL load_stats_1d for {config.iso2} from {config.date_from} to {config.date_to}.")    
+    context.log.info(
+        f"Running ETL load_stats_1d for {config['iso2']} from {config['date_from']} to {config['date_to']}.")
 
-    stats = get_stats_for_country(config.iso2, config.date_from, config.date_to, '1d')
+    stats = get_stats_for_country(config["iso2"], config["date_from"], config["date_to"], '1d')
     if stats:
-        insert_country_stats_to_db(config.iso2, '1d', stats, context.resources.db)
-        context.log.info(f"Successfully loaded {len(stats)} rows for {config.iso2} from {config.date_from} to {config.date_to}.")
+        insert_country_stats_to_db(config["iso2"], '1d', stats, context.resources.db)
+        context.log.info(
+            f"Successfully loaded {len(stats)} rows for {config['iso2']} from {config['date_from']} to {config['date_to']}.")
     else:
-        context.log.warning(f"No stats found for {config.iso2} from {config.date_from} to {config.date_to}.")
+        context.log.warning(f"No stats found for {config['iso2']} from {config['date_from']} to {config['date_to']}.")
 
 
 def insert_country_stats_to_db(country_iso2, resolution, stats, db_engine):
-    sql= "INSERT INTO data.country_stat(cs_country_iso2, cs_stats_timestamp, cs_stats_resolution, cs_v4_prefixes_ris," \
-         " cs_v6_prefixes_ris, cs_asns_ris, cs_v4_prefixes_stats, cs_v6_prefixes_stats, cs_asns_stats )\nVALUES "
+    sql = "INSERT INTO data.country_stat(cs_country_iso2, cs_stats_timestamp, cs_stats_resolution, cs_v4_prefixes_ris," \
+          " cs_v6_prefixes_ris, cs_asns_ris, cs_v4_prefixes_stats, cs_v6_prefixes_stats, cs_asns_stats )\nVALUES "
 
     for item in stats:
-        sql +=  (f"\n('{country_iso2}', '{item['timeline'][0]['starttime']}', '{resolution}', "
+        sql += (f"\n('{country_iso2}', '{item['timeline'][0]['starttime']}', '{resolution}', "
                 f"{item['v4_prefixes_ris'] if item['v4_prefixes_ris'] else 'NULL'}, "
                 f"{item['v6_prefixes_ris'] if item['v6_prefixes_ris'] else 'NULL'}, "
                 f"{item['asns_ris'] if item['asns_ris'] else 'NULL'}, "
@@ -41,6 +54,5 @@ def insert_country_stats_to_db(country_iso2, resolution, stats, db_engine):
                 f"{item['asns_stats'] if item['asns_stats'] else 'NULL'} ),")
     sql = sql[:-1] + ";"
 
-    with db_engine.begin() as conn: 
+    with db_engine.begin() as conn:
         conn.execute(text(sql))
-
