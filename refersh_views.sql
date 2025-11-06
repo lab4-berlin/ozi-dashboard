917942b18d68eb5f0cc8a6ae83caa1a2e90f0678
Ilya <ilya@lab4.berlin>
2025-11-06 17:18:25 +0100

    fixed view name

--- refersh_views.sql
+++ refersh_views.sql
@@ -1,4 +1,4 @@
 REFRESH MATERIALIZED VIEW data.vm_current_asn;
 REFRESH MATERIALIZED VIEW data.vm_asn_neighbour;
 REFRESH MATERIALIZED VIEW data.vm_connectivity_index_by_country;
-REFRESH MATERIALIZED VIEW data.vm_connectivity_index_by_asn;
+REFRESH MATERIALIZED VIEW data.vm_connectivity_index_by_asn_top10;
